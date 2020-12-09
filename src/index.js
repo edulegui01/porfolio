a4587bd3cc6f98d5f252530314699656368193aa
Santiago <edulegui01@gmail.com>
2020-12-08 23:14:24 -0300

    porfolio entero

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Routes from './Routes';
+import Porfolio from './components/Porfolio'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes></Routes>
+    <Porfolio></Porfolio>
   </React.StrictMode>,
   document.getElementById('root')
 );
